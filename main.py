57f3a1a408bbda26bae23a97b9b00fc0a32473e5
Mitchell McMillan <mitchelldmcmillan@gmail.com>
2014-03-26 20:02:52 +1100

    Basic video player layout
    Can you one line import using from?

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
-import setup
-from PyQt4 import QtCore, QtGui,uic
+import setup, sys
+from PyQt4 import QtCore, QtGui, uic
 
-uic.loadUi('video.ui')
+app = QtGui.QApplication(sys.argv)
+window = uic.loadUi('video.ui')
+window.show()
+sys.exit(app.exec_())
