400a1b213ba8cca2929aabe469152c2158148206
Mitchell McMillan <mitchelldmcmillan@gmail.com>
2014-03-27 13:19:29 +1100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,16 @@
 import setup, sys
 from PyQt4 import QtCore, QtGui, uic
 
+def playVideo(url):            
+    media = Phonon.MediaObject()
+    media.setCurrentSource(Phonon.MediaSource(url))
+    media.play()    
+
+def pauseVideo():
+    
+
 app = QtGui.QApplication(sys.argv)
 window = uic.loadUi('video.ui')
+QObject.connect(self.ppButton, QtCore.SIGNAL("clicked()"), playPause)
 window.show()
 sys.exit(app.exec_())
